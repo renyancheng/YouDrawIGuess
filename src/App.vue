8f20354851a78b96125cb38cef4e761fd8239c08
沐风 <2441260435@qq.com>
2021-08-16 09:35:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <n-layout>
+    <n-layout>
+      <n-layout-header>颐和园路</n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+      </n-layout-content>
+    </n-layout>
+    <n-back-top :right="40" :bottom="40" />
+  </n-layout>
+</template>
+
+<script setup>
+import { ref, h } from "vue";
+const state = ref("a");
+</script>
+
+<style scoped>
+.n-layout-header {
+  padding: 24px;
+}
+</style>
